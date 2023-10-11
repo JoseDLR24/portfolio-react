be784af5c88272f01ea52abbe8beb99c9d34d7db
Jose De Los Reyes <jddelosreyes23@hotmail.com>
2023-10-11 12:46:48 -0400

    header animation done

--- src/components/ui/header/header.jsx
+++ src/components/ui/header/header.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Header = () => {
+export default function Header() {
+    const [isScrollingUp, setIsScrollingUp] = useState(true);
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.scrollY;
+            setIsScrollingUp(prevScrollPos > currentScrollPos);
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [prevScrollPos]);
+
     return (
-        <header className="bg-blue-500 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-white text-2xl font-bold">Your Portfolio</h1>
-                <nav className="lg:flex lg:space-x-6">
-                    <a href="#home" className="text-white">Home</a>
-                    <a href="#about" className="text-white">About</a>
-                    <a href="#projects" className="text-white">Projects</a>
-                    <a href="#skills" className="text-white">Skills</a>
+        <header className={`comp-container min-h-fit fixed top-0 left-0 w-full transition-transform transform duration-300 ${isScrollingUp ? 'translate-y-0' : '-translate-y-full'} bg-white z-50`}>
+            <div className="content-container flex flex-col items-center space-y-1 py-5 border-b-2 border-teal-400 md:flex-row md:justify-between md:space-y-0">
+                <h1 className="text-2xl font-bold">JOSE.DEV</h1>
+                <nav className="flex flex-row space-x-5">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#skills">Skills</a>
                 </nav>
             </div>
         </header>
     );
 };
-
-export default Header;
