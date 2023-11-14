868d6abb340a36382aa7d1a5c88c255dcfb0f45c
Jose De Los Reyes <jddelosreyes23@hotmail.com>
2023-11-14 13:44:28 -0500

    mobile style changed

--- src/components/ui/header/header.jsx
+++ src/components/ui/header/header.jsx
@@ -26,69 +26,57 @@ export default function Header() {
 
 
     return (
-        <header className={`comp-container min-h-fit fixed top-0 left-0 w-full transition-transform transform duration-300 ${isScrollingUp ? 'translate-y-0' : '-translate-y-full'} bg-white z-50 shadow-lg`}>
-            <div className="flex flex-col items-center justify-center w-96 text-center md:w-7/12 px-3 md:px-0 space-y-2 py-2 md:py-5 md:flex-row md:justify-between md:space-y-0">
+        <header className={`comp-container min-h-fit fixed top-0 left-0 w-full transition-transform transform duration-300 ${isScrollingUp ? 'translate-y-0' : 'md:-translate-y-full'} bg-white z-50 shadow-lg`}>
+            <nav className="flex flex-row space-x-3 lg:space-x-6 py-6">
                 <Link
-                    to='home'
+                    to='about'
                     spy={true}
                     smooth={true}
                     offset={-50}
                     duration={1000}
-                    className="cursor-pointer text-2xl font-bold"
+                    className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
                 >
-                    JOSE.DEV
+                    About
                 </Link>
-                <nav className="flex flex-row space-x- lg:space-x-6">
-                    <Link
-                        to='about'
-                        spy={true}
-                        smooth={true}
-                        offset={-50}
-                        duration={1000}
-                        className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
-                    >
-                        About
-                    </Link>
-                    <Link
-                        to='skills'
-                        spy={true}
-                        smooth={true}
-                        offset={-50}
-                        duration={1000}
-                        className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
-                    >
-                        Skills
-                    </Link>
-                    <Link
-                        to='projects'
-                        spy={true}
-                        smooth={true}
-                        offset={-50}
-                        duration={1000}
-                        className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
-                    >
-                        Projects
-                    </Link>
-                    <Link
-                        to='contact'
-                        spy={true}
-                        smooth={true}
-                        offset={-50}
-                        duration={1000}
-                        className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
-                    >
-                        Contact
-                    </Link>
-                    <a
-                        href='https://drive.google.com/file/d/1l7qYj_9OTxbpABokssuqZlppBFYDiGVu/view?usp=share_link'
-                        target='_blank'
-                        rel='noopener noreferrer' // Recommended for security
-                        className='cursor-pointer font-bold underline decoration-wavy decoration-teal-400 p-2 transform hover:-translate-y-1 duration-500 ease-in-out hidden lg:flex'
-                    >
-                        Resume
-                    </a>
-                </nav>
-            </div>
+                <Link
+                    to='skills'
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={1000}
+                    className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
+                >
+                    Skills
+                </Link>
+                <Link
+                    to='projects'
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={1000}
+                    className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
+                >
+                    Projects
+                </Link>
+                <Link
+                    to='contact'
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={1000}
+                    className="cursor-pointer p-2 transform hover:-translate-y-1 duration-500 ease-in-out"
+                >
+                    Contact
+                </Link>
+                <a
+                    href='https://drive.google.com/file/d/1l7qYj_9OTxbpABokssuqZlppBFYDiGVu/view?usp=share_link'
+                    target='_blank'
+                    rel='noopener noreferrer' // Recommended for security
+                    className='cursor-pointer font-bold underline decoration-wavy decoration-teal-400 p-2 transform hover:-translate-y-1 duration-500 ease-in-out hidden lg:flex'
+                >
+                    Resume
+                </a>
+            </nav>
         </header>
     );
 };
